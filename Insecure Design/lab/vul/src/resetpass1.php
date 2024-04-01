8f06875b4e6e9f25e8e33b5add3fa3782d5e0ea1
khanhnq1611 <145059496+khanhnq1611@users.noreply.github.com>
2024-04-01 23:40:59 +0700

    Update resetpass1.php

--- Insecure Design/lab/vul/src/resetpass1.php
+++ Insecure Design/lab/vul/src/resetpass1.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 session_start();
-$db = new SQLite3('services/database.db');
+$db = new SQLite3('database.db');
 $error = false;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
